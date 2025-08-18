68ad81f5e344af58fb3551306e02da96a9f3feba
Hakan Akcinar <hakcinar1@gmail.com>
2025-08-18 14:52:51 +0300

    Route.ts type updates

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,17 @@
-import NextAuth from 'next-auth';
+// src/app/api/auth/[...nextauth]/route.ts
+import NextAuth, { AuthOptions } from 'next-auth';
 import Auth0Provider from 'next-auth/providers/auth0';
+import type { JWT } from 'next-auth/jwt';
 
-export const authOptions = {
+if (
+  !process.env.AUTH0_CLIENT_ID ||
+  !process.env.AUTH0_CLIENT_SECRET ||
+  !process.env.AUTH0_DOMAIN
+) {
+  throw new Error('Auth0 env değişkenleri tanımlı değil!');
+}
+
+export const authOptions: AuthOptions = {
   providers: [
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID,
@@ -12,7 +22,7 @@ export const authOptions = {
   session: { strategy: 'jwt' },
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async jwt({ token, account }) {
+    async jwt({ token, account }: { token: JWT; account?: any }) {
       if (account?.id_token) {
         const decoded = JSON.parse(
           Buffer.from(account.id_token.split('.')[1], 'base64').toString()
@@ -21,12 +31,13 @@ export const authOptions = {
       }
       return token;
     },
-    async session({ session, token }) {
-      session.user.roles = token.roles;
+    async session({ session, token }: { session: any; token: JWT }) {
+      if (session.user) session.user.roles = token.roles;
       return session;
     },
   },
 };
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
